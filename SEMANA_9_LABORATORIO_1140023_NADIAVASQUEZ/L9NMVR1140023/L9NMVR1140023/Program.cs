69dfb57b43bb60b27a36ee21f0e03bace2f2bc0a
nadiia116 <141685526+nadiia116@users.noreply.github.com>
2023-10-02 10:11:20 -0600

    Add files via upload

---
+++ SEMANA_9_LABORATORIO_1140023_NADIAVASQUEZ/L9NMVR1140023/L9NMVR1140023/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string texto = "";
+        if (n>0)
+        {
+            int contador =1;
+            int sumatoria = 0;
+
+            while (contador <= n)
+            {
+                texto = texto + contador + "+";
+                sumatoria= sumatoria + contador;
+                contador++;
+                
+            }
+            Console.WriteLine(texto);
+
+        }
+        
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("\nTabla del " + i + ":\t");
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+
