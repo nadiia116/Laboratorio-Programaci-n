326c724073d39390d945ef35d9fee54a88fb1cf5
nadiia116 <141685526+nadiia116@users.noreply.github.com>
2023-11-06 09:15:17 -0600

    Add files via upload

---
+++ SEMANA_14_LABORATORIO_CARNET_NADIAVASQUEZ/L14NMVR1140023/L14NMVR1140023/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Progrma
+{
+    static void Main(string[] args)
+    {
+        double[] vector = new double[8];
+
+        for (int i = 0; i < vector.Length; i++)
+        {
+            Console.WriteLine("Ingrese un  número");
+            vector[i] = int.Parse(Console.ReadLine());
+            if (vector[i] > 0)
+            {
+                Console.WriteLine(vector[i] + " es un numero postivo");
+            }
+            if (vector[i] < 0)
+            {
+                Console.WriteLine(vector[i] + " es un numero negativo");
+            }
+
+        }
+        
+    }
+}
+   
